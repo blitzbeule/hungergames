4d68f6d2e6667afa46b1f5c1de6d5676ea81d834
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2021-05-15 12:21:50 +0200

    Add debug jvm option for plugin hot-reload and debugging

--- setup/dev-setup.sh
+++ setup/dev-setup.sh
@@ -8,7 +8,7 @@ cd ../server/
 curl https://papermc.io/api/v1/paper/1.16.5/latest/download --output paper.jar
 
 echo "eula=true" > eula.txt
-echo "java -Xms${RAM}G -Xmx${RAM}G -jar paper.jar --nogui" > start.sh
+echo "java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Xms${RAM}G -Xmx${RAM}G -jar paper.jar --nogui" > start.sh
 chmod +x start.sh
 
 echo "Please stop the Server as it is fully started. Proceed with ENTER"
