2f89d069bf0681e41072a86c72d975a13e2776c5
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2021-05-15 15:46:34 +0200

    add untested setup scripts for windows

--- setup/dev-setup.bat
+++ setup/dev-setup.bat
@@ -1,1 +1,22 @@
-REM TBD
+@echo off
+
+set /p RAM="What is your available RAM? "
+
+cd ..
+mkdir server
+cd server
+curl https://papermc.io/api/v1/paper/1.16.5/latest/download --output paper.jar
+
+echo "eula=true" > eula.txt
+echo "java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Xms${RAM}G -Xmx${RAM}G -jar paper.jar --nogui" > start.bat
+
+set /p TEST="Please stop the Server as it is fully started. Proceed with ENTER"
+
+call start.bat
+
+cd ..
+cd setup
+
+call link.bat
+
+echo "Finished"
--- setup/link.bat
+++ setup/link.bat
@@ -1,1 +1,5 @@
-REM TBD
+cd ..
+
+mklink ..\..\plugins\core\target\hungergames-1.0-SNAPSHOT.jar server\plugins\hungergames.jar
+rd /s /q server\world\datapacks
+mklink ..\..\datapacks server\world\datapacks
