0e7a7a69627baac43e16b7ac1448403638be7534
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2021-05-17 19:10:16 +0200

    Add phase functionality

--- plugins/core/src/main/java/io/github/blitzbeule/hungergames/commands/setupCommand.java
+++ plugins/core/src/main/java/io/github/blitzbeule/hungergames/commands/setupCommand.java
@@ -1,6 +1,8 @@
 package io.github.blitzbeule.hungergames.commands;
 
 import io.github.blitzbeule.hungergames.Hungergames;
+import io.github.blitzbeule.hungergames.State;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -11,6 +13,30 @@ public class setupCommand extends CommandA{
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        String subcommand;
+        if (args.length == 0) {
+            subcommand = "-1";
+        } else {
+            subcommand = args[0];
+        }
+
+        switch (subcommand) {
+
+            case "start":
+                hg.getState().setPhase(State.GamePhase.SETUP);
+                return true;
+
+            case "finish":
+                //TODO check somehow if setup is actually finished
+                hg.getState().setPhase(State.GamePhase.PRE_GAME);
+                return true;
+
+            case "-1":
+            default:
+                sender.sendMessage(Component.text("You must specify a valid subcommand!"));
+                return false;
+
+        }
     }
 }
