926657534a98255290962d4034546733d9fdae2d
Paul Barbenheim <38007250+blitzbeule@users.noreply.github.com>
2021-05-29 11:42:52 +0200

    Fix issue

--- setup/link.sh
+++ setup/link.sh
@@ -2,6 +2,3 @@ cd ..
 ln -s ../../plugins/core/target/hungergames-1.0-SNAPSHOT.jar  server/plugins/hungergames.jar
 rm -rf server/world/datapacks
 ln -s ../../datapacks server/world/datapacks
-
-cd server/plugins
-curl https://dev.bukkit.org/projects/worldedit/files/3283695/download --output we.jar
